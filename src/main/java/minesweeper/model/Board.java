7004033596c5e886d3f18ec320f7246d45c1a515
Jere Mustonen <jere.mustonen@helsinki.fi>
2019-10-18 11:28:53 +0300

    adjacent incrementation

--- src/main/java/minesweeper/model/Board.java
+++ src/main/java/minesweeper/model/Board.java
@@ -38,7 +38,8 @@ public class Board {
         int col = 0;
         while (mineCount-- > 0) {
             this.board[row][col].isMine = true;
-            if (++col >= this.width){
+            this.incrementAdjacentSquares(row, col);
+            if (++col >= this.width) {
                 col = 0;
                 row++;
             }
@@ -49,11 +50,8 @@ public class Board {
      * Initializes the board with empty squares i.e. no mines.
      */
     public void initialize() {
-        this.board = Arrays.stream(board)
-        .map(row -> {
-            return Arrays.stream(row)
-            .map(squareElement -> new Square())
-            .toArray(Square[]::new);
+        this.board = Arrays.stream(board).map(row -> {
+            return Arrays.stream(row).map(squareElement -> new Square()).toArray(Square[]::new);
         }).toArray(Square[][]::new);
     }
 
@@ -61,10 +59,28 @@ public class Board {
     public String toString() {
         StringBuilder builder = new StringBuilder("Field \n");
         Arrays.stream(board).forEach(row -> {
-        Arrays.stream(row).forEach(square -> builder.append(square));
-        builder.append("\n");
-    });
+            Arrays.stream(row).forEach(square -> builder.append(square));
+            builder.append("\n");
+        });
         return builder.toString();
+    }
+
+    public void incrementAdjacentSquares(int x, int y) {
+        for(int xInc = -1; xInc <= 1; xInc++){
+            for(int yInc = -1; yInc <= 1; yInc++){
+                System.out.println(xInc + ":" + yInc);
+                if(withingBoard(x + xInc, y + yInc) && !(xInc == 0 && yInc == 0)){
+                    board[x + xInc][y + yInc].surrounding++;
+                }
+            }
+        }
+    }
+
+    private boolean withingBoard(int x, int y){
+        return x >= 0 
+        && x < this.width 
+        && y >= 0 
+        && y < this.length;
     }
 
 }
