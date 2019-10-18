b85270f1e7d7a1c3b53735f0592e1deaaf347bb2
Jere Mustonen <jere.mustonen@helsinki.fi>
2019-10-18 15:03:58 +0300

    documentation for Square class

--- src/main/java/minesweeper/model/Square.java
+++ src/main/java/minesweeper/model/Square.java
@@ -3,27 +3,46 @@ package minesweeper.model;
 
 /**
  * Represent a single square on the board.
+ * All the methods ragarding the information of this square, 
+ * i.e. amount of surrounding mines and whether this Square has a mine are only accessible if the square has been opened
  * @see model.Board
  */
 public class Square {
     private boolean isMine, opened = false;
-    private int surrounding = 0;
+    private int surrounding = 0; //Number of surrounding squares with mines
 
+    /**
+     * Generates new Square wih no mine
+     */
     public Square() {
     }
 
+    /**
+     * Generates a new Square as Square with mine.
+     */
     public Square(boolean isMine) {
         this.isMine = isMine;
     }
 
+    /**
+     * Represents a "click" on this particular square, and sets the state to opened.
+     */
     public void open() {
         this.opened = true;
     }
 
+    /**
+     * Wheter this Square is opened
+     * @return true if it is opened already, else false
+     */
     public boolean getOpen() {
         return this.opened;
     }
 
+    /**
+     * Wheter this Square is a Square with mine
+     * @return true if this is a mine Square and it has been opened already, false otherwise
+     */
     public boolean isMine() {
         if (this.opened) {
             return isMine;
@@ -31,7 +50,10 @@ public class Square {
 
         return false;
     }
-
+    /**
+     * amount of surrounding Squares that have a mine.
+     * @return 0 if this square has not been opened, otherwise the amount of surrounding Squares that have a mine.
+     */
     public int surrounding() {
         if (this.opened) {
             return surrounding;
@@ -40,10 +62,17 @@ public class Square {
         return 0;
     }
 
+    /**
+     * Increments the amount this Square has other Squares surrounding it, with mines in them
+     */
     public void incrementSurrounding() {
         this.surrounding++;
     }
 
+    /**
+     * Text representation for debugging purposes.
+     * @return String
+     */
     @Override
     public String toString() {
         if (!this.opened) return "X";
