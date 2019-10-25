b8c438ab91716ffa35facb61bd8988960198ba75
Sami Lahtinen <samsai@tutanota.com>
2019-10-25 11:48:08 +0300

    Fixed checkstyle and indentation

--- src/main/java/minesweeper/generator/MinefieldGenerator.java
+++ src/main/java/minesweeper/generator/MinefieldGenerator.java
@@ -10,8 +10,9 @@ public class MinefieldGenerator {
     private boolean isSafeArea(int currentX, int currentY, int safeOriginX, int safeOriginY) {
         for (int x = -1; x <= 1; x++) {
             for (int y = -1; y <= 1; y++) {
-                if (safeOriginX + x == currentX || safeOriginX + y == currentY)
+                if (safeOriginX + x == currentX || safeOriginX + y == currentY) {
                     return true;
+                }
             }
         }
         return false;
@@ -25,11 +26,11 @@ public class MinefieldGenerator {
 
         for (int x = -1; x <= 1; x++) {
             for (int y = -1; y <= 1; y++) {
-                if (isSafeArea(safeX + x, safeY + y , safeX, safeY) && board.withinBoard(safeX + x, safeY + y)){
+                if (isSafeArea(safeX + x, safeY + y , safeX, safeY) && board.withinBoard(safeX + x, safeY + y)) {
                     squaresLeft--;
+                }
             }
         }
-    }
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
