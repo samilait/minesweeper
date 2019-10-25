5faec3bb058b87b53701e3eef8ca32a5d89287d2
Jere Mustonen <jere.mustonen@helsinki.fi>
2019-10-25 11:41:02 +0300

    board safe area

--- src/main/java/minesweeper/generator/MinefieldGenerator.java
+++ src/main/java/minesweeper/generator/MinefieldGenerator.java
@@ -7,23 +7,41 @@ public class MinefieldGenerator {
     public MinefieldGenerator() {
     }
 
+    private boolean isSafeArea(int currentX, int currentY, int safeOriginX, int safeOriginY) {
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                if (safeOriginX + x == currentX || safeOriginX + y == currentY)
+                    return true;
+            }
+        }
+        return false;
+    }
+
     public void generate(Board board, int mines, int safeX, int safeY) {
         int width = board.length;
         int height = board.length;
 
-        int squaresLeft = width * height - 1;
+        int squaresLeft = width * height;
+
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                if (isSafeArea(safeX + x, safeY + y , safeX, safeY) && board.withinBoard(safeX + x, safeY + y)){
+                    squaresLeft--;
+            }
+        }
+    }
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 if (Math.random() < ((float) mines / squaresLeft)) {
-                    if (x == safeX && y == safeY) {
+                    if (isSafeArea(x, y, safeX, safeY)) {
                         continue;
                     }
 
                     board.board[x][y].setMine();
                     board.incrementAdjacentSquares(x, y);
                     mines--;
-                } 
+                }
 
                 squaresLeft--;
             }
