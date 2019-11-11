8f4ac4635e2a2179b5066b6855a6a19a5eacae11
Jere Mustonen <jere.mustonen@helsinki.fi>
2019-11-11 11:32:32 +0200

    added game seeding and board for bots

--- src/main/java/minesweeper/generator/MinefieldGenerator.java
+++ src/main/java/minesweeper/generator/MinefieldGenerator.java
@@ -1,3 +1,4 @@
+
 package minesweeper.generator;
 
 import java.util.ArrayList;
@@ -21,9 +22,12 @@ public class MinefieldGenerator {
         this.seedSet = true;
     }
 
+<<<<<<< 2db59276405679bffdde2dce2e704737780f1666
     /**
      * Checks if a given X,Y coordinate is within the safe area
      */
+=======
+>>>>>>> added game seeding and board for bots
     private boolean isSafeArea(int currentX, int currentY, int safeOriginX, int safeOriginY) {
         for (int x = -1; x <= 1; x++) {
             for (int y = -1; y <= 1; y++) {
