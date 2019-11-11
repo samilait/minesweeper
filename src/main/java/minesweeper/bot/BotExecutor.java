1e37694a96c238cc7970ab89a34b3e06f6a01535
Jere Mustonen <jere.mustonen@helsinki.fi>
2019-11-11 12:15:29 +0200

    added comments to code

--- src/main/java/minesweeper/bot/BotExecutor.java
+++ src/main/java/minesweeper/bot/BotExecutor.java
@@ -11,6 +11,9 @@ public class BotExecutor extends Thread {
     private Board board;
 
 
+    /**
+     * This class is used to encapsulate the bot to a thread so it can be run concurrently with the gui updater
+     */
     public BotExecutor(BlockingQueue<Move> moveQueue, Bot bot, Board board){
         this.queue = moveQueue;
         this.bot = bot;
@@ -21,13 +24,13 @@ public class BotExecutor extends Thread {
     public void run() {
         //Run bot while game has not ended
         while(!this.board.gameEnd){
-            //Try to add new move from bot to queue
+            // Try to add new move from bot to queue
             System.out.println("Moving");
+            // This is currently just for testing purposes to slown the bot down a bit 
             try{
                 this.sleep(100);
-            } catch (Exception e){
-
-            }
+            } catch (Exception e){}
+            // Bot makes moves to its own board and then the move is added to the supply queue
             Move move = this.bot.makeMove(this.board);
             this.queue.offer(move);
             this.board.makeMove(move);
