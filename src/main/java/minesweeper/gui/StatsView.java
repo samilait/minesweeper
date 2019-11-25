5e03dc7f540c4cfbf98e2e57e4f28d090c9c24b1
Sami Lahtinen <samsai@tutanota.com>
2019-11-25 12:26:52 +0200

    Use a FileChooser instead of hard-coded filename

--- src/main/java/minesweeper/gui/StatsView.java
+++ src/main/java/minesweeper/gui/StatsView.java
@@ -10,20 +10,29 @@ import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import javafx.animation.AnimationTimer;
 import javafx.collections.ObservableList;
+
 import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
+
 import minesweeper.model.GameStats;
 import javafx.animation.AnimationTimer;
+
 import java.text.DecimalFormat;
 import minesweeper.model.GameStats;
+
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class StatsView {
     private GameStats stats;
     private FileWriter logger;
     private Label exportStatus;
+    private Stage stage;
+
     public StatsView(GameStats stats) {
         this.stats = stats;
-        Stage stage = new Stage();
+        stage = new Stage();
 
         Label cumulativeDistance = new Label("Cumulative Distance:");
         Label cumulativeTime = new Label("Cumulative Time:");
@@ -60,8 +69,17 @@ public class StatsView {
     }
 
     private void logToFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export log");
+        fileChooser.getExtensionFilters().addAll(
+                new ExtensionFilter("Log files", "*.log"),
+                new ExtensionFilter("Log files", "*.txt"),
+                new ExtensionFilter("All files", "*.*"));
+        
+        File selectedFile = fileChooser.showSaveDialog(stage);
+
         try {
-            this.logger = new FileWriter("log.txt");
+            this.logger = new FileWriter(selectedFile);
             DecimalFormat numberFormat = new DecimalFormat("0.00");
             this.logger.write("Cumulative Distance: " + numberFormat.format(stats.cumulativeEuclidianDistance) + "\n");
             this.logger.write("Time: " + numberFormat.format(stats.cumulativeTime) + " sec\n");
@@ -71,7 +89,7 @@ public class StatsView {
             this.logger.flush();
             this.logger.close();
             this.exportStatus.setVisible(true);
-            this.exportStatus.setText("Statistics succesfully exported to file log.txt");
+            this.exportStatus.setText("Statistics exported to " + selectedFile.getPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
