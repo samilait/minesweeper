685e5198c29a20081fde6a9f48e4d87d44fca384
Sami Lahtinen <sami.lahtinen@helsinki.fi>
2019-11-29 12:30:31 +0200

    Added comments to the singleton class

--- src/main/java/minesweeper/StorageSingleton.java
+++ src/main/java/minesweeper/StorageSingleton.java
@@ -1,14 +1,27 @@
 package minesweeper;
 
+/**
+ * Singleton class for miscellaneous storage
+ * Used for transferring state between UI scenes without variable passing
+ */
 public class StorageSingleton {
+    // The instance is stored in a static, private variable
+    // This allows us to instantiate the singleton with getInstance()
     private static StorageSingleton singleton = new StorageSingleton();
 
+    // Animation speed
     public double animationSpeed = 1050.0;
 
+    /**
+     * Private constructor for creating one instance of the singleton
+     */
     private StorageSingleton() {
 
     }
 
+    /**
+     * Method for getting access to the singleton instance
+     */
     public static StorageSingleton getInstance() {
         return singleton;
     }
